c1029504e62c31a7c1b778b85f2e6d74b3eeaf98
ao-0316 <aosho0316.yukiusagi@gmail.com>
2024-02-04 08:34:58 +0000

    login

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,16 +1,36 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_customer!
+
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    # customer = Customer.find(params[:id])
+    customer = Customer.find(current_customer.id)
+    if customer.update(customer_params)
+      redirect_to public_customers_mypage_path, notice: "変更内容を保存しました。"
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
   end
 
   def withdraw
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_active: false)
+    reset_session
+    redirect_to public_top_path, notice: "退会処理を実行いたしました。"
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :address, :email)
   end
 end
--- app/controllers/public/yakitoris_controller.rb
+++ app/controllers/public/yakitoris_controller.rb
@@ -1,7 +1,18 @@
 class Public::YakitorisController < ApplicationController
+  
   def index
+    @yakitoris = Yakitori.all
+    @genres = Genre.all
+    #@yakitori_count = Yakitori.all
   end
 
   def show
+    @yakitori = Yakitori.find(params[:id])
+    @genres = Genre.all
   end
+  
+  def list_params
+    params.reguire(:yakitori).permit()
+  end  
+  
 end
