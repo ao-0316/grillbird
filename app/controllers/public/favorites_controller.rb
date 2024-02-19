536d900bc2d407ff29549dab4ee75a6c494582de
ao-0316 <aosho0316.yukiusagi@gmail.com>
2024-02-19 14:33:10 +0000

    favorites

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,17 @@
 class Public::FavoritesController < ApplicationController
 
   def create
-    yakitori = Yakitori.find(params[:yakitori_id])
-    favorite = current_customer.favorites.new(customer_id: current_customer.id, yakitori_id: yakitori.id)
+    @yakitori = Yakitori.find(params[:yakitori_id])
+    favorite = current_customer.favorites.new(customer_id: current_customer.id, yakitori_id: @yakitori.id)
     favorite.save
-    #redirect_to request.referer 
+    #redirect_to request.referer
   end
 
   def destroy
-    yakitori = Yakitori.find(params[:yakitori_id])
-    favorite = current_customer.favorites.find_by(yakitori_id: yakitori.id)
+    @yakitori = Yakitori.find(params[:yakitori_id])
+    favorite = current_customer.favorites.find_by(yakitori_id: @yakitori.id)
     favorite.destroy
-    #redirect_to request.referer 
+    #redirect_to request.referer
   end
 
 end
